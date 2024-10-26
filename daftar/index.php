4afa0b23751077bdd7c9675700bf898d9d4e730b
natasyahelmalia <natasyahelmalia@gmail.com>
2024-10-26 19:47:41 +0700

    feat: refactor list movies page

--- daftar/index.php
+++ daftar/index.php
@@ -1,32 +1,52 @@
 <?php 
 include '../koneksi.php';
 ?>
-DAFTAR FILM
-<table border="1">
-	<tr>
-		<th>No</th>
-		<th>Judul Film</th>
-		<th>Durasi</th>
-		<th>Kategori Film</th>
-	</tr>
-	<?php 
-	if(isset($_GET['cari'])){
-		$cari = $_GET['cari'];
-		$data = mysqli_query($con,"select * from film where judul_film like '%".$cari."%'");				
-	}else{
-		$data = mysqli_query($con,"select * from film");		
-	}
-	$no = 1;
-	while($d = mysqli_fetch_array($data)){
-	?>
-	<tr>
-		<td><?php echo $no++; ?></td>
-		<td><?php echo $d['judul_film']; ?></td>
-		<td><?php echo $d['durasi']; ?></td>
-		<td><?php echo $d['kategori_film']; ?></td>
-	</tr>
-	<?php } ?>
-</table>
-<a href="pesan_tiket.php">Pesan Tiket</a>
-</br>
-<a href="../">kembali</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Daftar Film</title>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<h2>Daftar Film</h2>
+		<table class="film-table">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Judul Film</th>
+					<th>Durasi</th>
+					<th>Kategori Film</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+				if (isset($_GET['cari'])) {
+					$cari = $_GET['cari'];
+					$data = mysqli_query($con, "SELECT * FROM film WHERE judul_film LIKE '%".$cari."%'");				
+				} else {
+					$data = mysqli_query($con, "SELECT * FROM film");		
+				}
+				
+				$no = 1;
+				while ($d = mysqli_fetch_array($data)) {
+				?>
+				<tr>
+					<td><?php echo $no++; ?></td>
+					<td><?php echo htmlspecialchars($d['judul_film']); ?></td>
+					<td><?php echo htmlspecialchars($d['durasi']); ?> Menit</td>
+					<td><?php echo htmlspecialchars($d['kategori_film']); ?></td>
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+		<div class="navigation">
+			<a href="../" class="back-button">Kembali</a>
+			<a href="pesan_tiket.php" class="btn">Pesan Tiket</a>
+		</div>
+	</div>
+</body>
+</html>
