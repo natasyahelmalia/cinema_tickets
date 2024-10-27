c0468a9152e70c19afb4844b7e9d9ae174fcf1be
natasyahelmalia <natasyahelmalia@gmail.com>
2024-10-27 19:49:14 +0700

    feat: enhancement booking page

--- daftar/pesan_tiket.php
+++ daftar/pesan_tiket.php
@@ -1,31 +1,48 @@
+<?php 
+include "../koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pesan Tiket</title>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style_pesan.css">
+</head>
 <body>
-<table border="1" class="table">
-	<tr>
-		<th>No</th>
-		<th>Judul Film</th>
-		<th>Studio</th>
-		<th>Jam Tayang</th>	
-	</tr>
-	<?php 
-	include "../koneksi.php";
-	$query_mysql = mysqli_query($con,"SELECT film.judul_film , jadwal.id_jadwal, jadwal.id_studio , jadwal.id_jam_tayang FROM film JOIN jadwal ON film.id_film = jadwal.id_film")or die(mysql_error());
-	$nomor = 1;
-	?>
-	<h3>Pesan Tiket</h3>
-	<?php
-	while($data = mysqli_fetch_array($query_mysql)){
-	?>
-	<tr>
-		<td><?php echo $nomor++; ?></td>
-		<td><?php echo $data['judul_film']; ?></td>
-		<td><?php echo $data['id_studio']; ?></td>
-		<td><a class="edit" href="detail_tiket.php?id_jadwal=<?php echo $data['id_jadwal']; ?>"><?php echo $data['id_jam_tayang']; ?></a></td>
-		
-		<td>
-		</td>
-	</tr>
-	<?php } ?>
-</table>
-<br>
-<a href="index.php">Kembali</a>
-<body>
+	<div class="container">
+		<h2>Pesan Tiket</h2>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Judul Film</th>
+					<th>Studio</th>
+					<th>Jam Tayang</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+				$query_mysql = mysqli_query($con, "SELECT film.judul_film, jadwal.id_jadwal, jadwal.id_studio, jadwal.jam_tayang 
+																					FROM film 
+																					JOIN jadwal ON film.id_film = jadwal.id_film") 
+																					or die(mysqli_error($con));
+				$nomor = 1;
+				while ($data = mysqli_fetch_array($query_mysql)) {
+				?>
+				<tr>
+					<td><?php echo $nomor++; ?></td>
+					<td><?php echo htmlspecialchars($data['judul_film']); ?></td>
+					<td><?php echo htmlspecialchars($data['id_studio']); ?></td>
+					<td><a class="link-detail" href="detail_tiket.php?id_jadwal=<?php echo $data['id_jadwal']; ?>"><?php echo htmlspecialchars($data['jam_tayang']); ?></a></td>
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+		<div class="navigation">
+				<a href="index.php" class="btn">Kembali</a>
+		</div>
+	</div>
+</body>
+</html>
