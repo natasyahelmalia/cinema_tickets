3593fafb21ef6fcab2071ff1b0af5e12b52f8def
natasyahelmalia <natasyahelmalia@gmail.com>
2024-10-27 22:36:10 +0700

    feat: update database sql

--- bioskop.sql
+++ bioskop.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2019 at 10:24 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.0.21
+-- Waktu pembuatan: 27 Okt 2024 pada 16.34
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,149 +24,195 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `film`
+-- Struktur dari tabel `film`
 --
 
 CREATE TABLE `film` (
   `id_film` int(11) NOT NULL,
   `judul_film` varchar(50) NOT NULL,
   `durasi` int(11) NOT NULL,
-  `kategori_film` enum('2D','3D') DEFAULT '2D'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `kategori_film` enum('Dewasa','Anak-anak') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `film`
+-- Dumping data untuk tabel `film`
 --
 
 INSERT INTO `film` (`id_film`, `judul_film`, `durasi`, `kategori_film`) VALUES
-(1, 'Dilan 1990', 113, '3D'),
-(2, 'Aquaman', 143, '2D'),
-(3, 'flight555', 116, '2D'),
-(4, 'MILLY & MAMET', 101, '2D'),
-(5, 'ASAL KAU BAHAGIA', 88, '2D');
+(1, 'Dilan 1990 (2018)', 113, 'Anak-anak'),
+(2, 'Aquaman (2023)', 143, 'Anak-anak'),
+(3, 'Izinkan Aku Berdosa (2024)', 116, 'Dewasa'),
+(4, 'Romeo dan Ronaldo (2000)', 101, 'Anak-anak'),
+(5, 'Saw VII Final Chapter (2007)', 88, 'Dewasa');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jadwal`
+-- Struktur dari tabel `harga`
+--
+
+CREATE TABLE `harga` (
+  `kategori` enum('Dewasa','Anak-anak') DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `harga`
+--
+
+INSERT INTO `harga` (`kategori`, `harga`) VALUES
+('Dewasa', 50000),
+('Anak-anak', 30000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwal`
 --
 
 CREATE TABLE `jadwal` (
   `id_jadwal` int(11) NOT NULL,
   `id_film` int(11) NOT NULL,
   `id_studio` enum('1','2','3','4','5') NOT NULL,
-  `id_jam_tayang` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `jam_tayang` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `jadwal`
+-- Dumping data untuk tabel `jadwal`
 --
 
-INSERT INTO `jadwal` (`id_jadwal`, `id_film`, `id_studio`, `id_jam_tayang`) VALUES
-(1, 1, '1', 10),
-(2, 1, '1', 12),
-(3, 1, '1', 15),
-(4, 1, '1', 16),
-(5, 3, '2', 21),
-(6, 3, '2', 19),
-(7, 5, '3', 17),
-(8, 5, '3', 14),
-(9, 2, '4', 20),
-(10, 2, '4', 15);
+INSERT INTO `jadwal` (`id_jadwal`, `id_film`, `id_studio`, `jam_tayang`) VALUES
+(1, 1, '1', '12:00'),
+(2, 1, '1', '13:30'),
+(3, 1, '1', '15:00'),
+(4, 1, '1', '16:30'),
+(5, 3, '2', '13:00'),
+(6, 3, '2', '14:30'),
+(7, 5, '3', '13:45'),
+(8, 5, '3', '15:00'),
+(9, 2, '4', '14:00'),
+(10, 2, '4', '15:30'),
+(11, 4, '5', '16.00'),
+(12, 4, '5', '17.30');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tiket`
+-- Struktur dari tabel `studio`
+--
+
+CREATE TABLE `studio` (
+  `id` int(11) NOT NULL,
+  `jumlah_kursi` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `studio`
+--
+
+INSERT INTO `studio` (`id`, `jumlah_kursi`) VALUES
+(1, 55),
+(2, 40),
+(3, 35),
+(4, 50),
+(5, 45);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tiket`
 --
 
 CREATE TABLE `tiket` (
   `id_tiket` int(11) NOT NULL,
   `id_jadwal` int(11) NOT NULL,
   `tanggal` date NOT NULL,
-  `jml_kursi` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tiket`
---
-
-INSERT INTO `tiket` (`id_tiket`, `id_jadwal`, `tanggal`, `jml_kursi`) VALUES
-(1, 0, '2019-01-05', 0),
-(2, 0, '2019-01-05', 0),
-(3, 0, '2019-01-05', 0),
-(4, 3, '2019-01-05', 1),
-(5, 3, '2019-01-05', 1),
-(6, 10, '2019-01-05', 2),
-(7, 1, '2019-01-05', 4);
+  `jml_kursi` int(5) NOT NULL,
+  `harga` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
   `id` int(10) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(255) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(0, 'admin', 'admin'),
-(1, 'admin', 'admin123');
+INSERT INTO `user` (`id`, `username`, `password`, `name`) VALUES
+(1, 'admin_natasya', 'admin123', 'Natasya Helmalia Putri');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `film`
+-- Indeks untuk tabel `film`
 --
 ALTER TABLE `film`
   ADD PRIMARY KEY (`id_film`);
 
 --
--- Indexes for table `jadwal`
+-- Indeks untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
   ADD PRIMARY KEY (`id_jadwal`);
 
 --
--- Indexes for table `tiket`
+-- Indeks untuk tabel `studio`
+--
+ALTER TABLE `studio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tiket`
 --
 ALTER TABLE `tiket`
   ADD PRIMARY KEY (`id_tiket`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `film`
+-- AUTO_INCREMENT untuk tabel `film`
 --
 ALTER TABLE `film`
   MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
--- AUTO_INCREMENT for table `jadwal`
+-- AUTO_INCREMENT untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
--- AUTO_INCREMENT for table `tiket`
+-- AUTO_INCREMENT untuk tabel `studio`
+--
+ALTER TABLE `studio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tiket`
 --
 ALTER TABLE `tiket`
-  MODIFY `id_tiket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;COMMIT;
+  MODIFY `id_tiket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
