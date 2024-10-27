eab19c02ece400cebf85ca324d0c2c9aa4dae70f
natasyahelmalia <natasyahelmalia@gmail.com>
2024-10-27 19:49:02 +0700

    feat: enhancement detal page

--- daftar/detail_tiket.php
+++ daftar/detail_tiket.php
@@ -1,61 +1,61 @@
-<html>
+<?php 
+include "../koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
 	<title>Detail Tiket</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style_detail.css">
 </head>
 <body>
-	
-	<br/>
-	
-	
- 
-	<br/>
-	<h3>Film</h3>
- 
-	<?php 
-	include "../koneksi.php";
-	
-	$id_jadwal = $_GET['id_jadwal'];
-	$query_mysql = mysqli_query($con,"SELECT * FROM jadwal WHERE id_jadwal='$id_jadwal'")or die(mysql_error());
-	$nomor = 1;
+	<div class="container">
+		<h3>Detail Tiket</h3>
 
-	$data = mysqli_fetch_array($query_mysql);
+		<?php 
+		$id_jadwal = $_GET['id_jadwal'];
+		$query_mysql = mysqli_query($con, "SELECT DISTINCT j.*, s.jumlah_kursi FROM jadwal j INNER JOIN studio s ON j.id_studio = s.id WHERE id_jadwal='$id_jadwal'") or die(mysqli_error($con));
+		$data = mysqli_fetch_array($query_mysql);
 
-	$id_film = $data['id_film'];
-	$query_mysql2 = mysqli_query($con,"SELECT * FROM film WHERE id_film= '$id_film'")or die(mysql_error());
-	$data2 = mysqli_fetch_array($query_mysql2);
-	
-	$jml_kursi=50;
-	?>
-	<form action="cetak_tiket.php?id_jadwal=<?php echo $data['id_jadwal']; ?>" method="post">		
-		<table border="1" class="table">
-			<tr>
-				<td>Film</td>
-				<td><?php $id_jadwal = $data['id_jadwal']; echo $data2['judul_film']; ?></td>					
-			</tr>	
-			<tr>
-				<td>Studio</td>
-				<td><?php echo $data['id_studio']; ?></td>					
-			</tr>	
-			<tr>
-				<td>Jam Tayang</td>
-				<td><?php echo $data['id_jam_tayang']; ?></td>					
-			</tr>	
-			<tr>
-				<td>Jumlah Kursi tersedia</td>
-				<td><?php echo $jml_kursi?></td>					
-			</tr>
-			<tr>
-				<td>Jumlah Tiket yang Dipesan</td>
-				<td><input name="jmltiket" type="number" id="jumlahtiket" required min="1"></td>					
-			</tr>
-			<tr>
-				
-				<td><a href="pesan_tiket.php">kembali </a></td>			
-				<td><input type="submit" value="Simpan"></td>					
-			</tr>
-		</table>
-	</form>
+		$id_film = $data['id_film'];
+		$query_mysql2 = mysqli_query($con, "SELECT * FROM film WHERE id_film='$id_film'") or die(mysqli_error($con));
+		$data2 = mysqli_fetch_array($query_mysql2);
+		?>
+
+		<form action="cetak_tiket.php?id_jadwal=<?php echo $data['id_jadwal']; ?>" method="post" class="detail-form">
+			<table class="table">
+				<tr>
+					<td>Film</td>
+					<td><?php echo htmlspecialchars($data2['judul_film']); ?></td>
+				</tr>	
+				<tr>
+					<td>Studio</td>
+					<td><?php echo htmlspecialchars($data['id_studio']); ?></td>
+				</tr>	
+				<tr>
+					<td>Jam Tayang</td>
+					<td><?php echo htmlspecialchars($data['jam_tayang']); ?></td>
+				</tr>	
+				<tr>
+					<td>Jumlah Kursi Tersedia</td>
+					<td><?php echo htmlspecialchars($data['jumlah_kursi']); ?></td>
+				</tr>
+				<tr>
+					<td>Jumlah Tiket yang Dipesan</td>
+					<td><input name="jmltiket" type="number" id="jumlahtiket" required min="1" placeholder="Masukkan jumlah tiket"></td>
+				</tr>
+				<tr class="form-actions">
+					<td colspan="2">
+						<div class="button-container">
+							<a href="pesan_tiket.php" class="btn">Kembali</a>
+							<input type="submit" value="Simpan" class="btn save-btn">
+						</div>
+					</td>
+				</tr>
+			</table>
+		</form>
+	</div>
 </body>
 </html>
